6cf27341439e52caf31f5335bab97baaa2d51f49
Cris Correa <cristina.correa.segade@gmail.com>
2025-06-27 10:42:19 +0100

    added logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,9 @@
 import daisiesGarden from "./daisiesGarden.svg";
+import logoHorizontal from "./logoHorizontal.svg";
+import logoSquare from "./logoSquare.svg";
 
 export {
-  daisiesGarden
+  daisiesGarden,
+  logoHorizontal,
+  logoSquare,
 }
